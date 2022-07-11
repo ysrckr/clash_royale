59a5df34bb63bd50d0ed33992d67670cc462680a
ysrckr <dyt.yasarcakir@gmail.com>
2022-07-11 21:25:36 +0300

    custom func totitlecase added

--- classes/CustomFunctions.js
+++ classes/CustomFunctions.js
@@ -2,7 +2,18 @@ class CustomFunctions {
 	constructor() {}
 
 	static capitalize(str) {
-		return str.charAt(0).toUpperCase() + str.slice(1)
+		const lower = str.toLowerCase()
+		return str.charAt(0).toUpperCase() + lower.slice(1)
+	}
+	static toTitleCase(str) {
+		const lower = str.toLowerCase()
+		return lower
+			.toLowerCase()
+			.split(' ')
+			.map(ltr => {
+				return ltr.charAt(0).toUpperCase() + ltr.substring(1)
+			})
+			.join(' ')
 	}
 }
 export default CustomFunctions
